8b68ca8f327373329fb64f2838dd6623e8e75f2d
anis-mondal <the.anismondal@gmail.com>
2026-05-28 12:43:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+
+// ES মডিউলের জন্য __dirname তৈরি করা হচ্ছে
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
