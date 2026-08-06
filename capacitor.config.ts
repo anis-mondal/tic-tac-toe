b509eae876129c1623c5fc3a226e48e55d97239e
Anis Mondal <the.anismondal@gmail.com>
2026-08-06 07:31:07 +0000

    Fixed webDir config and synced Capacitor plugins

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.game.tictactoe',
-  appName: 'Tic Tac Toe',
-  webDir: 'dist'
-};
-
-export default config;
