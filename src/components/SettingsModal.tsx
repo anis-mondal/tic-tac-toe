78202bb195dd9e20f13cecc2842e8909b3021f27
Anis Mondal <the.anismondal@gmail.com>
2026-09-10 10:15:38 +0530

    Import useState in SettingsModal component

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'motion/react';
 import { 
   X as CloseIcon, Info, Moon, Target, Check, Settings,
